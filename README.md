6ef84dd319255b6cde3b7c203c579ee064cacd8b
Feda Zidan <fido311@gmail.com>
2020-08-14 01:17:58 -0500

    start the Code Quize, created HTML and Style CSS

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Feda2020-CodeRepo-HW4
+Create Code Quiz
+
+## User Story
+Bootcamp student
+will take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT s/he can gauge their progress compared with their peers
+
+## Quiz Criteria
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
---
+++ script.js
@@ -0,0 +1,26 @@
+//This is an application of timed quize. 
+//The user click start the quize timer will start. 
+//The quize is a multiple choice
+
+//We need basic html elements.
+//Will need h1 Coding Quiz Challenge,header
+//Going to declare a var for h1 text
+//h2 try to answer....
+
+
+//When answered by the user
+//When user click on the answer
+//Click function, add an event listner
+
+
+// user will see correct or wrong 
+
+//next question will appear
+//Correct answer will move to the next question
+
+//wrong anwsers will penalized by subtracking 10 sc from the timer
+
+//once all questions answered the score will show
+//user will enter their info.
+//local storing the user info.
+//Show the highest score.
